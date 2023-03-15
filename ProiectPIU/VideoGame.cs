817ebec3812b75d6310a276976c640308f883b6e
Vlad Pintilie <nicuvlad95@hotmail.com>
2023-03-15 08:53:33 +0200

    Add project files.

---
+++ ProiectPIU/VideoGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPIU
+{
+    public class VideoGame
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Developer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+
+        public VideoGame(string title, string publisher, string developer, DateTime releaseDate, string genre, double price)
+        {
+            Title = title;
+            Publisher = publisher;
+            Developer = developer;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+            Price = price;
+        }
+    }
+}
---
+++ ProiectPIU/VideoGameStore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPIU
+{
+    public class VideoGameStore
+    {
+        public List<VideoGame> Games { get; set; }
+
+        public VideoGameStore()
+        {
+            Games = new List<VideoGame>();
+        }
+
+        public void AddGame(VideoGame game)
+        {
+            Games.Add(game);
+        }
+
+        public void RemoveGame(VideoGame game)
+        {
+            Games.Remove(game);
+        }
+
+        public List<VideoGame> SearchGamesByTitle(string keyword)
+        {
+            return Games;
+        }
+
+        public List<VideoGame> SearchGamesByGenre(string genre)
+        {
+            return Games;
+        }
+
+        public List<VideoGame> SearchGamesByPrice(double minPrice, double maxPrice)
+        {
+            return Games;
+        }
+    }
+}
